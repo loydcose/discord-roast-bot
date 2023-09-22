22ec96de2baa9f6772e03b73c88c9bd175c92752
LAPTOP-7IMPE5DI\loydc <loydcose.01@gmail.com>
2023-09-22 19:37:02 +0800

    clean up

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Filter = require("bad-words")
 const filipinoBadwords = require("filipino-badwords-list")
-const filter = new Filter({ list: filipinoBadwords.array })
-
-const dotenv = require("dotenv")
-dotenv.config()
-
 const { Client, GatewayIntentBits } = require("discord.js")
 const { replies } = require("./replies")
+const dotenv = require("dotenv")
+
+const filter = new Filter({ list: filipinoBadwords.array })
+dotenv.config()
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -15,25 +15,8 @@ const client = new Client({
   ],
 })
 
-// console.log(process.env.DISCORD_BOT_TOKEN_API)
-
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`)
-})
-
-// Respond to messages
-client.on("message", (message) => {
-  try {
-    if (message.author.bot) return
-
-    // Check if the message starts with "hello"
-    if (message.content.toLowerCase() === "hello") {
-      console.log(message.content)
-      message.reply("Hello, world!")
-    }
-  } catch (error) {
-    console.error("Error handling message:", error)
-  }
 })
 
 client.on("messageCreate", (message) => {
@@ -47,14 +30,6 @@ client.on("messageCreate", (message) => {
       content: "selectedMessage",
     })
   }
-
-  // const selectedMessage =
-  //   replies[Math.floor(Math.random() * replies.length - 1)] || "bonak"
-
-  // message.reply({
-  //   content: selectedMessage,
-  // })
 })
 
-// Login to Discord with your app's token
 client.login(process.env.DISCORD_BOT_TOKEN_API)
